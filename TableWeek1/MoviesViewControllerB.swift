f672e0edcc498bf03bcf88cc4bd17b86a0963262
Kent <54918251+kentomagento@users.noreply.github.com>
2022-02-09 00:35:46 -0800

    week1_final

---
+++ TableWeek1/MoviesViewControllerB.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesViewControllerB.swift
+//  TableWeek1
+//
+//  Created by kentmacmini on 2/8/22.
+//
+
+import Foundation
+import UIKit
+
+class MoviesViewControllerB: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("hello world!")
+        
+    }
+}
