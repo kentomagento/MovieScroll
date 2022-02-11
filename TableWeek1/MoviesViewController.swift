09c5240e815765e5081ea2400d46de67c3288c3f
Kent <itskent@protonmail.com>
2022-02-10 22:24:24 -0800

    "menu"

--- TableWeek1/MoviesViewController.swift
+++ TableWeek1/MoviesViewController.swift
@@ -74,4 +74,18 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        
+        //find selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        //pass selected to view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
